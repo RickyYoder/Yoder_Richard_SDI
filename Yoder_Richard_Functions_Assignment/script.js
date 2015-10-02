f2fc80a01224f08b18937df3696186942d20390c
RickyYoder <rickyayoder15@gmail.com>
2015-10-01 21:21:43 -0400

    Adding Necessary HTML
    Adding necessary HTML elements to manipulate within JavaScript.

---
+++ Yoder_Richard_Functions_Assignment/script.js
@@ -0,0 +1,28 @@
+//declare variables
+
+var form = document.forms['calc'], //the form
+	numbers = form.numbers, //our numbers input
+	result = document.getElementById('result'), //our result to be displayed
+	list = []; //our list of numbers
+
+function avg(){ //our function that calculates the average
+	var nums = numbers.value.split(" "); //creates an array from our number input value
+	for(var x = 0; x < nums.length; x++){
+		if(isNaN(Number(nums[x]))) nums[x] = 0; //if the value isn't a number, just set it to 0
+		else nums[x] = Number(nums[x]); //otherwise, make it a number
+	}
+
+	list = nums;
+	
+	return list;
+}
+
+form.addEventListener('submit',function(event){ //when the form is submitted
+	event.preventDefault();
+	event.stopPropagation();
+	//^^just to keep the form from submitting^^
+
+	avg(); //calculate the average
+
+	return 0;
+});
