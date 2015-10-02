95e6009fa9c7e2123763ca6198a1b98690896722
RickyYoder <rickyayoder15@gmail.com>
2015-10-01 21:43:14 -0400

    Final Touches
    Added round parameter to the avg function, adjusted variable names to
    prevent conflicts.

--- Yoder_Richard_Functions_Assignment/script.js
+++ Yoder_Richard_Functions_Assignment/script.js
@@ -2,10 +2,11 @@
 
 var form = document.forms['calc'], //the form
 	numbers = form.numbers, //our numbers input
+	roundOutput = form.roundOutput, //a checkbox to determine whether or not to round our output to the nearest whole number
 	result = document.getElementById('result'), //our result to be displayed
 	list = []; //our list of numbers
 
-function avg(){ //our function that calculates the average
+function avg(round){ //our function that calculates the average
 	var nums = numbers.value.split(" "); //creates an array from our number input value
 	for(var x = 0; x < nums.length; x++){
 		if(isNaN(Number(nums[x]))) nums[x] = 0; //if the value isn't a number, just set it to 0
@@ -14,7 +15,7 @@ function avg(){ //our function that calculates the average
 
 	list = nums;
 
-	var avg = 0,
+	var Avg = 0,
 		count = list.length,
 		val = 0;
 
@@ -22,11 +23,13 @@ function avg(){ //our function that calculates the average
 		val += list[x]; //calculate sum
 	}
 
-	avg = val/count; //divide sum of values by number of values to get average
+	Avg = val/count; //divide sum of values by number of values to get average
 
-	result.innerText = 'The average value from your set of numbers is '+avg; //display result to user
+	if(round) Avg = Math.round(Avg); //round average only if supposed to
+
+	result.innerText = 'The average value from your set of numbers is '+Avg; //display result to user
 	
-	return avg;
+	return Avg;
 }
 
 form.addEventListener('submit',function(event){ //when the form is submitted
@@ -34,7 +37,7 @@ form.addEventListener('submit',function(event){ //when the form is submitted
 	event.stopPropagation();
 	//^^just to keep the form from submitting^^
 
-	avg(); //calculate the average
+	avg(roundOutput.checked); //calculate the average
 
 	return 0;
 });
